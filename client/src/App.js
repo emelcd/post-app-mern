e1f03793cb3f4005b8c64983f4c7fefd60d46742
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-26 23:26:57 +0100

    Virginia's tricks

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid, Portal } from "@material-ui/core";
 
-
+// import memories from './images/memories.png';
 const App = () => {
-    return (
-        <div>
-            <h1>cryptoPOST</h1>
-        </div>
-    )
-}
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+
+      </AppBar>
+        <Portal container=>
+          <span>But I actually render here!</span>
+        </Portal>
+    </Container>
+  );
+};
 export default App;
