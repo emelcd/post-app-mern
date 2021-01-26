558574be0774c3a8013e158cf056ac3c8a8ecbd8
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-26 03:54:35 +0100

    Added Connection to MONGODBATLAS

--- server/index.js
+++ server/index.js
@@ -3,4 +3,21 @@ import bodyparser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const app = express();
 
+app.use(bodyparser.json({ limit: "30mb", extended: true }));
+app.use(bodyparser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://mongoRoot:e9mGxYQxiWPth2Vk@cluster0.549lo.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5700;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server RUnning on: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set(("useFindAndModify", false));
+mongoose.set('useFindAndModify', false);
