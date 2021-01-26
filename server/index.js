861e63184b901d541153f6df6e7ed2cfb1c4271c
mickiticki <70339302+mickiticki@users.noreply.github.com>
2021-01-26 04:49:11 +0100

    Added Routes Logic -createFolders routes, controllers

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyparser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyparser.json({ limit: "30mb", extended: true }));
 app.use(bodyparser.urlencoded({ limit: "30mb", extended: true }));
@@ -11,8 +15,8 @@ app.use(cors());
 
 const CONNECTION_URL =
   "mongodb+srv://mongoRoot:e9mGxYQxiWPth2Vk@cluster0.549lo.mongodb.net/<dbname>?retryWrites=true&w=majority";
-
-const PORT = process.env.PORT || 5700;
+ 
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
